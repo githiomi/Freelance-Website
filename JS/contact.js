e0a70e922e4822d853ba3aa74d157e3548a35ae9
githiomi <danielgithiomi@gmail.com>
2022-12-26 23:16:33 +0400

    Correted Linking and added animation

---
+++ JS/contact.js
@@ -0,0 +1,9 @@
+// Reveal Animation
+const scrollReveal = ScrollReveal({
+    distance: '50px',
+    duration: 2500,
+    delay: 200,
+    reset: true
+});
+
+scrollReveal.reveal('#contactImg', {origin:'left'});
